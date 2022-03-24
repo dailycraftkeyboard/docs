10ca1a2efadedc12271b9ebc1ea09505146b8cea
yfuku <fukumoto@gmail.com>
2022-03-24 15:11:58 +0900

    update

--- assets/js/18.612eac35.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[18],{471:function(t,n,s){"use strict";s.r(n);var e=s(56),i=Object(e.a)({},(function(){var t=this.$createElement,n=this._self._c||t;return n("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[n("p",[this._v("wings42")])])}),[],!1,null,null,null);n.default=i.exports}}]);
